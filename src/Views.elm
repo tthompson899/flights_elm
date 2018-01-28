9a150936da209037608a0e6939feb9acd81e3da6
Tiffany Thompson <tdthompson899@gmail.com>
2018-01-28 06:10:41 -0600

    added heading that displays Flight Tracker and DFW

--- src/Views.elm
+++ src/Views.elm
@@ -1,7 +1,7 @@
 module Views exposing (..)
 
 import Html exposing (..)
-import Html.Attributes exposing (class, title, id, style)
+import Html.Attributes exposing (..) --class, title, id, style
 import Models exposing (..)
 import Messages exposing (..)
 --import Utility exposing (durationFormat, timeOfDay, getOrElse)
@@ -22,19 +22,37 @@ resetCardStyle height margin =
 
 --styleWidth width =
     --style [ ( "width", width ) ]
+    
+h3Style: List ( String, String )
+h3Style = 
+    [ ("margin-right", "45px")
+    , ("margin-top" , "10px")
+    
+    ]
 
 
+topHeadingStyle : List ( String, String )
+topHeadingStyle = 
+    [ ("background-color", "#4D4D4D")
+    , ("color", "#FEFEFE")
+    , ("height", "90px")
+    , ("margin", "0")
+    , ("padding-left", "40px")
+    , ("padding-top", "12px")
+    , ("margin-bottom", "51px")
+    ]
+    
 fullWidthStyle : List ( String, String )
 fullWidthStyle =
-    [ ( "width", "100%" )
+    [ ( "width", "10%" )
     , ( "padding", "0" )
     , ( "margin", "0" )
     , ( "height", "100vh" )
     ]
 
-
 mainLayout : Html Msg -> Html Msg
 mainLayout content =
+
     div [ class "mainwrapper", style fullWidthStyle ]
         [ div [ class "container", style fullWidthStyle ]
             [ div
@@ -74,7 +92,7 @@ arrivalsView model =
                 |> List.map (\flight -> faline flight)
 
     in
-        table []
+        table [ class "bordered" ]
             [ thead []
                 [ tr []
                     [ th [] [ text "Depature City" ]
@@ -85,12 +103,27 @@ arrivalsView model =
                     ]
                 ]
             , tbody []
-                arrivals
+--         comment out so I can test adding stuff to table arrivals
+                [ tr []
+                    [ td [] [ text "some stuff" ]]
+                
+                ]
             ]
+        
 
 view : Model -> Html Msg
 view model =
-    div [ ]
-        [ h1 [style [("color", "red")]] [ text "Arrivals" ]
-        , arrivalsView model
-        ]
+        div []
+            [ div [class "topPageStyle", style topHeadingStyle]
+                [ img [src "../img/send_icon.png", style [ ("height", "50px"), ("width", "50px")]] [ ]
+                ,  h3 [ style [("display", "inline-block"), ("margin-top", "10px")]] [ (text "Flight Tracker") ]  
+                ,  h3 [ class "right", style h3Style] [( text "| DFW" )]
+                ]
+--                , arrivalsView model
+                div [ style [("border-color", "green")]]
+--                [ div []
+--                    
+--                ]
+                
+            ]
+              
